6b4d0632bc56003dd657ee805ba3838f62982869
Brian Kwon <bkwon94@gmail.com>
2021-08-30 19:57:19 -0400

    finish adventure challenge

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getStoryChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getStoryChoice2(), for: .normal)
 
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = sender.currentTitle!
+        storyBrain.getNextStory(userChoice: choice)
+        updateStory()
+    }
+    
+    func updateStory() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        let choice1 = storyBrain.getStoryChoice1()
+        let choice2 = storyBrain.getStoryChoice2()
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
+    }
+    
 }
 
